2c3a18a075437e56f8d4d9eadeebf852e5e65965
DariaRyzhova <daria.ryzhova@mail.ru>
2012-11-20 01:38:24 +0400

    Update morphomon/preprocessing/corpus-dialog.py
    Поправила регулярку и добавила правило про то, что точка после тэга аббревиатуры (abbrev) не обозначает конец предложения

--- morphomon/preprocessing/corpus-dialog.py
+++ morphomon/preprocessing/corpus-dialog.py
@@ -1,16 +1,23 @@
-﻿import codecs, re
+import codecs, re
 
-f1 = codecs.open('rueval-tagged.txt', 'r', 'utf-8')
+f1 = codecs.open('rueval_2010_goldstandard_tagged.txt', 'r', 'utf-8')
 f2 = codecs.open('rueval-our-version.txt', 'w', 'utf-8')
 
+brev = 0
 for line in f1:
-    m = re.search(u'([А-я]+?[-]?[А-я]*?)\t(.*?)\t(.*?)\t(.*?)\t', line)
+    m = re.search(u'([А-яЁё]+?[-]?[А-яЁё]*?)\t(.*?)\t(.*?)\t(.*?)\t', line)
     m1 = re.search(u'[\.?!]', line)
+    m2 = re.search(u'.*abbrev.*', line)
+    if m1 != None:
+        if brev != 1:
+            f2.write(u'\n')
+    if m2 != None:
+        brev = 1
+    else:
+        brev = 0
     if m != None:
         stroka_new = m.group(1)+'\t'+m.group(2)+'='+m.group(3)+','+m.group(4)+'\r\n'
         f2.write(stroka_new)
-    elif m1 != None:
-        f2.write(u'\r\n')
-
+        
 f1.close()
 f2.close()

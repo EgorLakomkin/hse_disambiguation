8fb527ea3d7169f1d3ecb441993025943bdf6e3f
EgorLakomkin <egor.lakomkin@gmail.com>
2012-11-26 18:42:53 +0400

    fix bug in sintagrus parser

--- morphomon/preprocessing/syntagrus.py
+++ morphomon/preprocessing/syntagrus.py
@@ -7,7 +7,7 @@ def get_token_str_rep(el):
     lemma = el.attrib["lemma"]
     word_form = el.text
     lemma_str = u"{0}={1}".format( lemma, ','.join(gram.split(' ')) )
-    result_str = u"{0}\t{1}\r\n".format(word_form,'\t'.join(lemma_str))
+    result_str = u"{0}\t{1}\r\n".format(word_form,lemma_str)
     return result_str
 
 def process_syntagrus_file( file, outfile ):
@@ -24,5 +24,3 @@ def process_syntagrus_file( file, outfile ):
 
     except Exception as e:
         raise Exception("Dom parsing exception %s" % (e))
-
-process_syntagrus_file( settings.CORPUS_DATA_ROOT + 'Anketa.tgt', settings.CORPUS_DATA_ROOT + 'processed_anketa.txt' )

0c58bf22842953408ecf03c427db7e241834bd17
EgorLakomkin <egor.lakomkin@gmail.com>
2012-12-05 11:14:18 +0400

    Исправленная статистика

--- morphomon/algorithm/statistics.py
+++ morphomon/algorithm/statistics.py
@@ -1,57 +1,56 @@
 # -*- coding: utf-8 -*-
+from collections import defaultdict
 from morphomon.utils import get_tokens_from_corpora, get_word_ending
 import settings
 
 
 
 def calculate_A(corpus_file):
+    """
+    Возвращаем матрицу переходов состояний A и начальный вектор распределения p
+    """
     tokens = get_tokens_from_corpora(corpus_file)
 
-    A_matrix = {}
-
+    A = defaultdict(lambda: defaultdict(float))
+    p = defaultdict(float)
     prev_token = None
     for token in tokens:
         gram = token.gram
         if prev_token:
-            if prev_token.gram not in  A_matrix:
-                A_matrix[prev_token.gram] = {}
-            if gram not in A_matrix[prev_token.gram]:
-                A_matrix[prev_token.gram][gram] = 1
-            else:
-                A_matrix[prev_token.gram][gram] += 1
+            A[prev_token.gram][gram] += 1
+        else:
+            p[gram] +=1
+
         prev_token = token
-    return A_matrix
+    return A,p
 
 def calculate_B(corpus_file):
+    """
+    Считаем матрицу наблюдений B
+    """
     tokens = get_tokens_from_corpora(corpus_file)
 
-    B_matrix = {}
+    B = defaultdict(lambda: defaultdict(float))
     #ключ - окончание слова, значение - словарь с грамматическими формамими : грам.форма => кол-во раз встреч в корпусе
 
     for token in tokens:
         word_form = token.word
         gram = token.gram
         ending = get_word_ending(word_form,enging_length=4)
-        if ending not in B_matrix:
-            B_matrix[ending] = {}
+        B[gram][ending] += 1
 
-        if gram not in B_matrix[ending]:
-            B_matrix[ending][gram] = 1
-        else:
-            B_matrix[ending][gram] += 1
-
-    return B_matrix
+    return B
 
 
 if __name__=="__main__":
     B_matrix = calculate_B(corpus_file = settings.CORPUS_DATA_ROOT + 'processed_anketa.txt')
-    A_matrix = calculate_A(corpus_file = settings.CORPUS_DATA_ROOT + 'processed_anketa.txt')
+    A_matrix,p = calculate_A(corpus_file = settings.CORPUS_DATA_ROOT + 'processed_anketa.txt')
 
     gram_pr = set()
     for key in B_matrix:
-        for gram_form in B_matrix[key]:
-            gram_pr.add(gram_form)
-            print "Окончание",key,"грам форма", gram_form,B_matrix[key][gram_form]
+        for ending in B_matrix[key]:
+            gram_pr.add(key)
+            print "Окончание",ending,"грам форма", key,B_matrix[key][ending]
 
     print "Размерность множества окончаний", len(B_matrix)
     print "Размерность множества грам. признаков", len(gram_pr)

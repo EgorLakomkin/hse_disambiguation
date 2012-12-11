43ccf5211bb9db4508b9d136d78c44b812ccd1d3
Daria Ryzhova <daria.ryzhova@mail.ru>
2012-12-12 00:05:28 +0400

    крошечные усовершенствования старых программ

--- morphomon/preprocessing/lemmer_starter.py
+++ morphomon/preprocessing/lemmer_starter.py
@@ -1,22 +1,24 @@
 ﻿import codecs, re, os
 from subprocess import Popen
 
-f = codecs.open('rueval-our-version.txt', 'r', 'utf-8')
-fOut = codecs.open('./lemmer/texts/clean/rueval.txt', 'w', 'utf-8')
-fOut.write('<?xml version="1.0" '
-            'encoding="utf-8"?>\r\n'
-            '<html><body>\r\n')
-for line in f:
-    m = re.search(u'([А-яЁё]+?[-]?[А-яЁё]*?)\t', line)
-    if m != None:
-        stroka = m.group(1) + u'\r\n'
-        fOut.write(stroka)
-fOut.write('</body>\r\n</html>')
-fOut.close()
+corpora = ['processed_anketa.txt', 'processed_opencorpora.txt', 'processed_fiction.txt']
+for i in corpora:
+    f = codecs.open(i, 'r', 'utf-8')
+    fOut = codecs.open('./lemmer/texts/clean/' + i[10:], 'w', 'utf-8')
+    fOut.write('<?xml version="1.0" '
+                'encoding="utf-8"?>\r\n'
+                '<html><body>\r\n')
+    for line in f:
+        m = re.search(u'([А-яЁё]+?[-]?[А-яЁё]*?)\t', line)
+        if m != None:
+            stroka = m.group(1) + u'\r\n'
+            fOut.write(stroka)
+    fOut.write('</body>\r\n</html>')
+    fOut.close()
 
-do_bat_path = os.path.abspath('./lemmer/processing')
-p = Popen(do_bat_path + '/do.bat', cwd=do_bat_path)
-stdout, stderr = p.communicate()
+    do_bat_path = os.path.abspath('./lemmer/processing')
+    p = Popen(do_bat_path + '/do.bat', cwd=do_bat_path)
+    stdout, stderr = p.communicate()
 
-f.close()
+    f.close()
 

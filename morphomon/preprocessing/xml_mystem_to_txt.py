7a4230eba2b509f05c5571ae1cc59b703c5ce0e9
EgorLakomkin <egor.lakomkin@gmail.com>
2012-12-16 20:48:28 +0400

    RusCorpora parser

--- morphomon/preprocessing/xml_mystem_to_txt.py
+++ morphomon/preprocessing/xml_mystem_to_txt.py
@@ -1,41 +1,67 @@
-﻿import re
-import codecs
-
-#токен\tлемма1=список грамм признаков\tлемма2=                                
+﻿import codecs
+import lxml.html
 
 
-m = u'(<w>.*?</w>)'
-m1 = u'<ana lex="(.*?)" gr="(.*?)"></ana>'
-m2 = u'.*</ana>(.*)</w>'
-n = u'</se>'
+import xml.parsers.expat
 
-corpora = ['anketa.xml', 'opencorpora.xml', 'fiction.xml', 'rueval.xml']
+#код позаимстован отсюда:
+#https://github.com/irokez/Pyrus/blob/master/src/rnc.py
+class Reader:
+    def __init__(self):
+        self._parser = xml.parsers.expat.ParserCreate()
+        self._parser.StartElementHandler = self.start_element
+        self._parser.EndElementHandler = self.end_element
+        self._parser.CharacterDataHandler = self.char_data
 
-for i in corpora:
-    f = codecs.open(i, 'r','cp1251')
-    o = codecs.open(i[:-4] + '_mystem.txt', 'w', 'utf-8')
-    data = f.readlines()
+    def start_element(self, name, attr):
+        if name == 'ana':
+            self._info = attr
 
-    for i in range(0,len(data)):
-        s = re.search(m,data[i])
-        s1 = re.search (n, data [i])
-        if s != None:
-            s2 = re.search(m2, s.group(1))
-            if s2 != None:
-                o.write(s2.group(1) + u'\t')
-            s3 = re.findall(m1, s.group(1))
-            if s3 != None:
-                for j in range(0,len(s3)):
-                    para = s3[j]
-                    lemma = para[0]
-                    gram = para[1]
-                    string = lemma + '=' + gram + '\t'
-                    o.write(string)
-            o.write('\r\n')
+    def end_element(self, name):
+        if name == 'se':
+            self._sentences.append(self._sentence)
+            self._sentence = []
+        elif name == 'w':
+            self._sentence.append((self._cdata, self._info))
+        elif name == 'ana':
+            self._cdata = ''
 
-        if s1 != None:
-            o.write('\r\n')
-                                
-    f.close()
-    o.close()
+    def char_data(self, content):
+        self._cdata += content
 
+    def read(self, filename, codepage):
+        f = codecs.open( filename, 'r', codepage )
+        content = f.read().encode('utf-8')
+        f.close()
+
+        self._sentences = []
+        self._sentence = []
+        self._cdata = ''
+        self._info = ''
+
+        self._parser.Parse(content)
+
+        return self._sentences
+
+def process_ruscorpora_file( file, outfile ):
+    out_f =  codecs.open( outfile, 'w', 'utf-8' )
+    try:
+        sentences = Reader().read( file, "cp1251" )
+        for sentence in sentences:
+            for word in sentence:
+                accent = word[0].index('`') + 1 if '`' in word[0] else 0
+                word_form = word[0].replace('`', '')
+                lemma = word[1]['lex']
+                gram = word[1]['gr']
+                lemma_str = u"{0}={1}".format( lemma, ','.join(gram.split(' ') ) )
+                result_str = u"{0}\t{1}\r\n".format(word_form,lemma_str)
+                out_f.write(result_str)
+            out_f.write('\r\n')
+
+    except Exception as e:
+        raise Exception("Dom parsing exception %s" % (e))
+
+if __name__ == "__main__":
+    in_file_test = "/home/umka/distr/ruscorpora/post1950/anecdota/gostin.xhtml"
+    out_file = "/home/umka/gostin_test.txt"
+    process_ruscorpora_file(in_file_test, out_file)

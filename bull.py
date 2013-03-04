68ee12b9efb01385cc5c1b4a5b9fa21113bc3407
Egor Lakomkin <egor.lakomkin@gmail.com>
2013-03-04 16:15:13 +0400

    bull.py for eval

--- bull.py
+++ bull.py
@@ -2,7 +2,8 @@
 import os
 import sys
 import traceback
-from morphomon.utils import create_dir
+from morphomon.eval import M_strict_mathcher, P_no_garbage, calculate_dir_precision
+from morphomon.utils import create_dir, load_object, remove_directory_content, remove_ambiguity_file_list, N_rnc_pos
 
 FOLDS_DIRNAME = 'folds'
 TEST_FILENAME = 'test.txt'
@@ -11,9 +12,8 @@ TEST_DIR = 'test'
 TRAIN_DIR = 'train'
 TRAIN_FILENAME = 'train.txt'
 
-def bull(action, experiment_name, experiment_path, fold):
+def bull(action, experiment_name, experiment_path, fold, gold = None):
     experiment_dir = os.path.join( experiment_path, experiment_name  )
-    fold_dir = os.path.join(experiment_dir, FOLDS_DIRNAME, fold )
 
 
     sys.path.append( os.path.abspath(experiment_path) )
@@ -32,22 +32,45 @@ def bull(action, experiment_name, experiment_path, fold):
         'experiment_dir' : experiment_dir
     }
     if action == 'train':
+        fold_dir = os.path.join(experiment_dir, FOLDS_DIRNAME, fold )
+
         train_files_source_dir = os.path.join(experiment_dir, TRAIN_DIR)
         fold_train_file = os.path.join( fold_dir, TRAIN_FILENAME )
         params['fold'] = fold
         params['train_file_list'] = [os.path.abspath(os.path.join(train_files_source_dir,line.strip())) for line in open(fold_train_file, 'r').readlines()]
+
+        mod.runner( **params )
+
     elif action == 'test':
+        fold_dir = os.path.join(experiment_dir, FOLDS_DIRNAME, fold )
+
         test_files_source_dir = os.path.join(experiment_dir, TEST_DIR)
         fold_test_file = os.path.join( fold_dir, TEST_FILENAME )
-        params['fold'] = fold
-        params['test_file_list'] = [os.path.abspath(os.path.join(test_files_source_dir,line.strip())) for line in open(fold_test_file, 'r').readlines()]
+        test_files= [os.path.abspath(os.path.join(test_files_source_dir,line.strip())) for line in open(fold_test_file, 'r').readlines()]
 
         result_dir =  os.path.join( fold_dir, 'test_result' )
         create_dir( result_dir )
-        params['result_dir'] =result_dir
 
-    mod.runner( **params )
+        model_filename = 'model_{0}.dat'.format( fold )
+        model_filename = os.path.abspath(os.path.join(experiment_dir,model_filename))
+        if not os.path.exists( model_filename ):
+            print >>sys.stderr, "Model has not been trained for experiment {0} for fold {1}".format(experiment_name, fold )
+        hmm_algo = load_object( model_filename )
+        remove_directory_content( result_dir )
+        remove_ambiguity_file_list( ambig_filelist = test_files, output_dir=result_dir, algo=hmm_algo)
+        print "Finished removing ambiguity for experiment {0} for fold {1}".format(experiment_name, fold )
+    elif action == 'eval':
+        #результат работы алгоритма в конкретном фолде
+        fold_dir = os.path.join(experiment_dir, FOLDS_DIRNAME, fold )
 
+        errors_filename =  os.path.join( fold_dir, 'errors.txt' )
+
+        algo_dir = os.path.join( fold_dir, 'test_result' )
+
+        test_files_source_dir = os.path.join(experiment_dir, TEST_DIR)
+
+        calculate_dir_precision( algo_dir = algo_dir, gold_dir = gold,ambi_dir=test_files_source_dir, M=M_strict_mathcher,
+        N=N_rnc_pos, P = P_no_garbage, errors_context_filename=errors_filename)
 
 
 
@@ -58,6 +81,8 @@ if __name__ == "__main__":
     parser.add_argument('-e', '--experiment_name', help="Name of the experiment")
     parser.add_argument('-p', '--experiment_path', help="Path to the experiment")
     parser.add_argument('-f', '--fold', help="fold number")
+    parser.add_argument('-g', '--gold', help="gold directory")
+
 
     args = parser.parse_args()
 
@@ -71,10 +96,14 @@ if __name__ == "__main__":
         print >>sys.stderr, "You did not specify experiment path"
         exit()
 
-    if not args.fold:
-        print >>sys.stderr, "You did not specify fold"
+    if (args.action in ['train','test'])  and not args.fold:
+        print >>sys.stderr, "You did not specify fold for {0} action".format( args.action )
+        exit()
+
+    if args.action == 'eval' and not args.gold:
+        print >>sys.stderr, "You did not specify gold directory"
         exit()
 
     bull(action=args.action, experiment_name=args.experiment_name,
-         experiment_path=args.experiment_path, fold = args.fold)
+         experiment_path=args.experiment_path, fold = args.fold, gold = args.gold)
 
